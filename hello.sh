6790f1ab11122820383bd3d7d130148f3cd32bbc
gad <nday.gady@gmail.com>
2024-09-04 23:22:44 -0700

    shell scripts nov

---
+++ hello.sh
@@ -0,0 +1,14 @@
+uthor : Utrains
+    #Date : 01-Nov-2021
+
+## ---------- script that display hello word in the screen -----------------
+
+echo "Hello Word !"
+
+#sleep 3
+
+echo "Hello gad how are you doing?"
+#sleep 3
+echo " your kernel version is:"
+uname -r
+#sleep 3
---
+++ pkg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+    #Author : Gad ndayishimiye
+    #Date : 01-Nov-2024
+
+## ---------- script that Install some packages in Linux -----------------
+
+yum install finger -y 
+yum install curl -y 
+yum install zip -y 
+yum install vim -y 
+
